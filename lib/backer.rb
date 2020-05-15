bdce00d465a4d59d48164cfa96de188788b44669
Paul Beckwith <303dog@gmail.com>
2020-05-15 19:04:48 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,14 @@
 class Backer
   
-  attr_reader :name, :backed_projects 
-  
+  attr_accessor :name, :backed_projects
+
   def initialize(name)
     @name = name
     @backed_projects = []
   end
-  
-  def backed_project(project)
-      @backed_projects << project
-      project.add_backer(self) unless project.backers.include?(self)
-  end 
 
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
+  end
 end
